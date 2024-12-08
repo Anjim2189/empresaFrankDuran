a7bebc3cf14ad0f62f8750779574af7a5d287ab7
Anjim2189 <jimenezdj21@gmail.com>
2024-12-08 13:03:30 +0100

    Update db.js

--- db.js
+++ db.js
@@ -3,11 +3,14 @@ const { Pool } = require('pg');
 
 // Configuración de la conexión a la base de datos PostgreSQL
 const pool = new Pool({
-  user: 'postgres',        // Nombre de usuario de PostgreSQL
-  host: '@ep-nameless-flower-a59va4el.us-east-2.aws.neon.tech/Empresa?sslmode=require',         // Servidor donde está la base de datos (localhost para tu máquina local)
+  user: 'Empresa_owner',        // Nombre de usuario de PostgreSQL
+  host: 'ep-nameless-flower-a59va4el.us-east-2.aws.neon.tech',         // Servidor donde está la base de datos (localhost para tu máquina local)
   database: 'Empresa', // Nombre de la base de datos
-  password: '5565', // Contraseña de tu usuario
-  port: 5432,                // Puerto de conexión, el predeterminado es 5432
+  password: 'pAY7yelr0IHP', // Contraseña de tu usuario
+  port: 5432,                // Puerto de conexión, el predeterminado es 5432*/
+  ssl: {
+    rejectUnauthorized: false, // Si es necesario para conexiones seguras
+  }
 });
 
 module.exports = pool;
